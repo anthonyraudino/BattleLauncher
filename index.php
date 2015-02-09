ff1a86efa7f924396e818fb2e5d659233eb3b4f8
plygnd <anthonyraudino@me.com>
2015-02-09 19:38:19 +1100

    Final push for 09022015

--- index.php
+++ index.php
@@ -3,13 +3,11 @@ $URL = 'origin.com';
 $ping = @fsockopen ($URL, 80, $errno, $errstr, 10);
 (!$ping) ? $status = $URL.' is down' : $status = $URL.' is up';
 
-echo $status.'<br>';
 
 $URL = 'battlelog.battlefield.com';
 $ping = @fsockopen ($URL, 80, $errno, $errstr, 10);
-(!$ping) ? $status = $URL.' is down' : $status = $URL.' is up';
+(!$ping) ? $status = $URL.' is down' : $status2 = $URL.' is up';
 
-echo $status;
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,6 +27,10 @@ echo $status;
 <img class=".logostyle" src="logo-optimised.png" />
 </div>
 <a href="https://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"></a>
+<div id="ping">
+<?php echo $status.'<br>';
+echo $status2; ?>
+</div>
 
 
 </body>
